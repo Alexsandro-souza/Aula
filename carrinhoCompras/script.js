aafcc9c588d1b5d4e7b6e4478acd360374a99ded
Alexsandro souza <alexsandrosouzasilva68@gmail.com>
2023-05-30 23:03:56 -0300

    Correção da função soma

--- carrinhoCompras/script.js
+++ carrinhoCompras/script.js
@@ -6,14 +6,21 @@ const msSucesso = document.getElementById('success');
 let precoCadas = document.querySelector('.precoCdas')
 let produtoCdas = document.querySelector('.produtoCdas');
 let cadastrado = document.querySelector('.cadastrado')
+let total = document.getElementById('total');
+
+
+
 
 // Arrys
 let miniBanco = new Array;
+produto.focus();
+
 
 // Funções
 btnCadas.addEventListener('click',()=>{
     let produtoV = produto.value
     let precoV = preco.value
+    
     
     
     if(produto.value !== '' && preco.value !== ''){
@@ -23,36 +30,61 @@ btnCadas.addEventListener('click',()=>{
                 produto : produtoV,
                 preco : precoV
             });
-        
- 
 
-        cadastrado.innerHTML = "";
-        miniBanco.map((cadastros)=>
-        {  
+        cadastrado.innerHTML = "";        
+        let soma = 0;  
+        
+        miniBanco.map((cadastros)=>{
+            soma = soma + parseFloat(cadastros.preco);
             cadastrado.innerHTML +=`
             <div class="containerCadast">
             <h3 class="produtoCdas">`+cadastros.produto+`</h3>
-            <h3 class="precoCdas">R$ `+cadastros.preco+`</h3>
-            <input type="button" value="Deletar">
+            <h3 class="precoCdas" > R$ `+cadastros.preco+`</h3>
+            <input type="button" class="deletar" value="Deletar">
             </div>`
+            total.innerText = `Total R$ ${soma}`;
         })
+        
+
+            // console.log(del)
+        
+            
         // Limpa o input e dá foco input produto
 
             produto.value = "";
             preco.value = "";
             produto.focus();
+            
+            
         
         // mensagem de sucesso para o usuário
         msSucesso.style.display='block';
         setTimeout(() => {
             msSucesso.style.display='none';
         }, 1000);
-
+        
+        
 
     }else{
         alert('Digite o produto e o valor para realizar o cadastro!')
     }
 
 })
+        document.addEventListener('click',(e)=>{
+            const elemento = e.target; //o target permite ver o elemento
+            const elPai = elemento.parentNode; //essa proriedade mostra quem é o elemento pai
+
+            if(elemento.classList.contains('deletar')){
+                elPai.remove();
+            }
+            
+        })
+                
+    
+
+    // let del = document.getElementById('deletar')
+    // console.log(del)
+
+
 
 
